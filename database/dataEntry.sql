2a90a08ce8621df979b9f24acf5a866dd32e15b0
Arghya Biswas <arghyabiswas05@gmail.com>
2026-02-07 23:50:29 +0530

    Fix Add Expense not wokring issue
    Signed-off-by: Arghya Biswas <arghyabiswas05@gmail.com>

--- database/dataEntry.sql
+++ database/dataEntry.sql
@@ -2,3 +2,9 @@
 INSERT INTO categories(name) VALUES
 ('Electronics'),
 ('Others');
+
+-- default accounts
+INSERT INTO accounts(name) VALUES
+('Cash'),
+('UPI'),
+('Bank');
--- web/backend.php
+++ web/backend.php
@@ -6,24 +6,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = isset($_POST['action']) ? $_POST['action'] : '';
     $tx_id = isset($_POST['tx_id']) ? intval($_POST['tx_id']) : 0;
 
-    if ($action === 'edit' && $tx_id > 0) {
+    if ($action === 'add') {
         $date = isset($_POST['date']) ? $_POST['date'] : '';
         $type = isset($_POST['type']) ? $_POST['type'] : '';
         $amount = isset($_POST['amount']) ? floatval($_POST['amount']) : 0;
         $category = isset($_POST['category']) ? intval($_POST['category']) : 0;
+        $account = isset($_POST['account']) ? intval($_POST['account']) : 0;
+        $note = isset($_POST['note']) ? $_POST['note'] : '';
+
+        $stmt = $db->prepare(
+            "INSERT INTO transactions (date, type, amount, category_id, account_id, note, payment_mode)
+             VALUES (?, ?, ?, ?, ?, ?, 'cash')"
+        );
+        $stmt->bindValue(1, $date, SQLITE3_TEXT);
+        $stmt->bindValue(2, $type, SQLITE3_TEXT);
+        $stmt->bindValue(3, $amount, SQLITE3_FLOAT);
+        $stmt->bindValue(4, $category, SQLITE3_INTEGER);
+        $stmt->bindValue(5, $account, SQLITE3_INTEGER);
+        $stmt->bindValue(6, $note, SQLITE3_TEXT);
+        $stmt->execute();
+    }
+    elseif ($action === 'edit' && $tx_id > 0) {
+        $date = isset($_POST['date']) ? $_POST['date'] : '';
+        $type = isset($_POST['type']) ? $_POST['type'] : '';
+        $amount = isset($_POST['amount']) ? floatval($_POST['amount']) : 0;
+        $category = isset($_POST['category']) ? intval($_POST['category']) : 0;
+        $account = isset($_POST['account']) ? intval($_POST['account']) : 0;
         $note = isset($_POST['note']) ? $_POST['note'] : '';
 
         $stmt = $db->prepare(
             "UPDATE transactions
-             SET date=?, type=?, amount=?, category_id=?, note=?
+             SET date=?, type=?, amount=?, category_id=?, account_id=?, note=?
              WHERE id=?"
         );
         $stmt->bindValue(1, $date, SQLITE3_TEXT);
         $stmt->bindValue(2, $type, SQLITE3_TEXT);
         $stmt->bindValue(3, $amount, SQLITE3_FLOAT);
         $stmt->bindValue(4, $category, SQLITE3_INTEGER);
-        $stmt->bindValue(5, $note, SQLITE3_TEXT);
-        $stmt->bindValue(6, $tx_id, SQLITE3_INTEGER);
+        $stmt->bindValue(5, $account, SQLITE3_INTEGER);
+        $stmt->bindValue(6, $note, SQLITE3_TEXT);
+        $stmt->bindValue(7, $tx_id, SQLITE3_INTEGER);
         $stmt->execute();
     }
     elseif ($action === 'delete' && $tx_id > 0) {
